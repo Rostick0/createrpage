0db4560716658d6e032729958737534baa14b178
Rostislav <rostik057@gmail.com>
2022-09-19 22:39:16 +0300

    fix text db

--- logic/include/connect.php
+++ logic/include/connect.php
@@ -1,6 +1,6 @@
 <?
 
-$database = new mysqli('localhost', 'root', '', 'company');
+$database = new mysqli('localhost', 'root', 'root', 'company');
 
 if (!$database) {
     die('Нет подключения к бд');
